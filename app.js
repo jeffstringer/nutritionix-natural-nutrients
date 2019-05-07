f119a0cc83c131869c975c3854e5f5fd6e8647b9
Jeff Stringer <jeff.j.stringer@gmail.com>
2019-05-07 07:51:31 -0700

    move main logic to it's own function/file

--- app.js
+++ app.js
@@ -1,34 +1,8 @@
-const request = require('request')
-const argv = require('yargs').argv
-const Food = require('./models/food')
+const program = require('yargs').argv
+const query = require('./query')
 
 if (argv.query) {
-  const options = {
-    url: 'https://trackapi.nutritionix.com/v2/natural/nutrients',
-    form: {query: argv.query, locale: 'en_US'},
-    headers: {
-      'x-app-id': process.env.NUTRITIONIX_APP_ID,
-      'x-app-key': process.env.NUTRITIONIX_APP_KEY,
-      'x-remote-user-id': '0'
-    }
-  }
-
-  function callback(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      let foodData = JSON.parse(response.body)['foods']
-      foods = []
-      foodData.forEach((foodDatum) => {
-        let food = new Food(foodDatum)
-        foods.push(food)
-      })
-      console.table(foods)
-    } else {
-      console.log('status', response.statusCode)
-      console.log('error', error)
-    }
-  }
-
-  request.post(options, callback)
+  query.apiCall(argv.query)
 } else {
   console.log('Please supply a query')
 }
---
+++ query.js
@@ -0,0 +1,36 @@
+const prog = require('yargs').argv
+const Food = require('./models/food')
+const request = require('request')
+
+let apiCall = (query) => {
+  let options = {
+    url: 'https://trackapi.nutritionix.com/v2/natural/nutrients',
+    form: {query: argv.query, locale: 'en_US'},
+    headers: {
+      'x-app-id': process.env.NUTRITIONIX_APP_ID,
+      'x-app-key': process.env.NUTRITIONIX_APP_KEY,
+      'x-remote-user-id': '0'
+    }
+  }
+
+  function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      let foodData = JSON.parse(response.body)['foods']
+      foods = []
+      foodData.forEach((foodDatum) => {
+        let food = new Food(foodDatum)
+        foods.push(food)
+      })
+      console.table(foods)
+    } else {
+      console.log('status', response.statusCode)
+      console.log('error', error)
+    }
+  }
+
+  request.post(options, callback)
+}
+
+module.exports = {
+  apiCall
+}
